6ba1c7b7f0a2fd244f19ed58934a41718c68e286
Jon Pabico <jbpabico@gmail.com>
2014-12-09 18:09:50 -0800

    controller tests for users and posts

--- social/test/controllers/users_controller_test.rb
+++ social/test/controllers/users_controller_test.rb
@@ -4,4 +4,24 @@ class UsersControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get new with new user" do
+    get :new
+    user = assigns(:user)
+    assert user.new_record?
+    assert_response :success
+  end
+
+  test "should create user" do 
+    params = {
+        user: {
+            email: "user@example.com", 
+            password: "password",
+            password_confirmation: "password"
+        }
+    }
+
+    post :create, params
+    assert_redirected_to root_url
+  end
 end
