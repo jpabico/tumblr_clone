4f54198487b6040e906af99976ec1da5bc39ae3f
Jon Pabico <jbpabico@gmail.com>
2014-12-16 04:48:06 -0800

    added seed file for sample data

--- social/db/seeds.rb
+++ social/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+alice = User.create(name: "Alice", email: "alice@example.com ", password: "password", password_confirmation: "password")
+bob = User.create(name: "Bob", email: "bob@example.com", password: "password", password_confirmation: "password")
+carol = User.create(name: "Carol", email: "carol@example.com", password: "password", password_confirmation: "password")
+dave = User.create(name: "Dave", email: "dave@example.com", password: "password", password_confirmation: "password")
+
+alice.text_posts.create(title: "My 1st post", body: "I never thought this would happen")
+alice.image_posts.create(title: "My first picture", url: "http://i.imgur.com/Yq81mMU.gif")
+bob.text_posts.create(title: "My first post", body: "now what?")
+bob.image_posts.create(title: "So saxy", url: "http://i.imgur.com/18SaeQ1.jpg")
+bob.text_posts.create(title: "a 2nd text post", body: "testing 1 2 3")
+carol.text_posts.create(title: "Post number 1", body: "where's number 2")
+carol.image_posts.create(title: "So saxy", url: "http://i.imgur.com/NjN3P3W.jpg")
+carol.text_posts.create(title: "Post number 2", body: "where's number 3")
+dave.text_posts.create(title: "Post 1 is here", body: "do you care?")
+dave.text_posts.create(title: "a 2nd text post", body: "testing 456")
+
